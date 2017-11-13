1565024d0f54524d636a042ea4d9f3e174e2343d
FilipStefaniuk <filip.stefaniuk@gmail.com>
2017-11-13 22:45:58 +0100

    client

---
+++ client/src/main/java/pl/edu/mimuw/cloudatlas/client/ServletDemo.java
@@ -0,0 +1,21 @@
+package pl.edu.mimuw.cloudatlas.client;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/ServletDemo")
+public class ServletDemo extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>");
+        out.println("Works.");
+        out.println("</body></html>");
+    }
+}
---
+++ client/src/main/webapp/static/js/test.js
@@ -0,0 +1,7 @@
+
+
+$(document).ready(function() {
+    setInterval(function() {
+        $("#time").load("ajax/test")
+    }, 2000);
+});
